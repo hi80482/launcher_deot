5b6936e972f19d1bf58a0fb5cdb67772bb422b3a
hi80482 <66435094+hi80482@users.noreply.github.com>
2020-10-17 00:16:21 +0800

    icelaglace:patch-1
    No hangup on boot sound

--- load.sh
+++ load.sh
@@ -5,7 +5,7 @@ BASENAME=`dirname "$0"`
 cd $BASENAME/sys.py
 
 # boot sound
-mplayer -volume 75 /home/cpi/music/startup.mp3
+nohup mplayer -volume 75 /home/cpi/music/startup.mp3
 
 # set system volume
 amixer sset 'Master' 50%
